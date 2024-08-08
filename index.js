5e1a3a2d7d1220e9d3845891417ae32e3b9a1595
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:25:17 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Get started!
+
+let MLB = prompt("what's your favorite MLB team?")
+
+console.log("welcome to the game," + MLB)
+
+let role = prompt("what role do you want?(outfielder, starting pitcher,Shortstop)")
+
+console.log(`okay, ${MLB},your role is now outfielder${role}.`)
+
+
+let baseballLevel = prompt("what is your position(1-9)")
+
+
+baseballLevel = parsenInt(baseballLevel)
+
+const numberofplayersPI = 3.1415927
